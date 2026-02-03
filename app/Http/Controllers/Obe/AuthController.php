68151875c5deba321afeef9b659d8aae3d2f0ce3
imam <you@example.com>
2026-02-03 14:05:56 +0800

    selsai untuk view nilai

--- app/Http/Controllers/Obe/AuthController.php
+++ app/Http/Controllers/Obe/AuthController.php
@@ -52,7 +52,7 @@ class AuthController extends Controller
     public function me(Request $request)
     {
         $dosen = $request->user()->select(
-            'kode_dosen',
+            // 'kode_dosen',
             'nama_dosen',
             'field_studi',
             'alumni',
@@ -61,7 +61,7 @@ class AuthController extends Controller
             'status_dosen',
             'program_studi.nama_program_studi',
             'alamat_email',
-            'status_login',
+            // 'status_login',
         )
             ->join('program_studi', 'dosen.homebase', '=', 'program_studi.kode_program_studi', 'left')
             ->first();
