eaf7fc0754d381bd1d6fe6b1400799806d46d798
imam <you@example.com>
2026-02-05 10:31:53 +0800

    asdf

--- app/Http/Controllers/Api/Obe/PenilaianObeController.php
+++ app/Http/Controllers/Api/Obe/PenilaianObeController.php
@@ -38,30 +38,39 @@ class PenilaianObeController extends Controller
 
     public function kelas(Request $request)
     {
-        // Ambil user dari Sanctum session/cookie auth
-        $user = $request->user();
-        if (! $user instanceof Dosen) {
-            return response()->json([
-                'status' => false,
-                'message' => 'Unauthenticated',
-            ], 401);
-        }
-
-        $kode_dosen = $user->getKey();
+        $plainTextToken = $request->bearerToken();
+        $accessToken = PersonalAccessToken::findToken($plainTextToken);
+        $dosen = $accessToken->tokenable;
+        $kode_dosen = $dosen->getKey();
 
         $data = Kelas::select(
             'kode_dosen',
-            'semester',
             'kelas.kelas_id as code_kelas',
+            'matakuliah.nama_matakuliah',
             'kelas.nama_kelas_id',
             'nama_kelas',
-            'id_matakuliah',
-        )->with('matakuliah')
+            'matakuliah.id_matakuliah',
+            DB::raw('COUNT(kelas_mahasiswa.kelas_id) as jumlah_mahasiswa')
+        )
             ->join('mengajar', 'kelas.kelas_id', '=', 'mengajar.kelas_id')
             ->join('nama_kelas', 'kelas.nama_kelas_id', '=', 'nama_kelas.nama_kelas_id')
+            ->join('matakuliah', 'kelas.id_matakuliah', '=', 'matakuliah.id_matakuliah')
+            ->join('kelas_mahasiswa', 'kelas.kelas_id', '=', 'kelas_mahasiswa.kelas_id')
+            ->join('krs_detail', 'kelas_mahasiswa.kode_krs_detail', '=', 'krs_detail.kode_krs_detail')
+            ->join('krs', 'krs_detail.kode_krs', '=', 'krs.kode_krs')
             ->where('mengajar.kode_dosen', $kode_dosen)
             ->where('kelas.kode_tahun_akademik', $this->TahunAkademik)
-            ->limit(10)
+            ->whereRaw('SUBSTR(krs.nim, 1, 2) > 24')
+            ->groupBy(
+                'kode_dosen',
+                'kelas.semester',
+                'kelas.kelas_id',
+                'kelas.nama_kelas_id',
+                'nama_kelas',
+                'matakuliah.id_matakuliah',
+                'matakuliah.nama_matakuliah'
+            )
+            ->having('jumlah_mahasiswa', '>', 0)
             ->get()
             ->map(function ($item) {
                 $item->code_kelas = Crypt::encryptString($item->code_kelas);
@@ -88,7 +97,7 @@ class PenilaianObeController extends Controller
 
         return response()->json([
             'status' => true,
-            'data' => $request->all(),
+            'data' => $data,
         ], 200);
     }
 }
